9e7d42cea50e7f44703561ab384863788998f7f6
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-21 22:47:06 -0500

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -5,7 +5,7 @@ class block_carousel_educo_edit_form extends block_edit_form {
     protected function specific_definition($mform) {
         global $CFG;
 
-        $itemsnumber = 3;
+        $itemsnumber = 1;
         if (isset($this->block->config->itemsnumber)) {
             $itemsnumber = $this->block->config->itemsnumber;
         }
@@ -14,20 +14,28 @@ class block_carousel_educo_edit_form extends block_edit_form {
 
         $itemsrange = range(1, 5);
         $mform->addElement('select', 'config_itemsnumber', get_string('config_itemsnumber', 'block_carousel_educo'), $itemsrange);
-        $mform->setDefault('config_itemsnumber', 3);
+        $mform->setDefault('config_itemsnumber', 1);
 
         for ($i = 1; $i <= 5; $i++) {
             $mform->addElement('header', 'config_edash_item' . $i, get_string('config_item', 'block_carousel_educo') . " $i");
 
             $mform->addElement('text', 'config_item_title' . $i, get_string('config_title', 'block_carousel_educo', $i));
-            $mform->setDefault('config_item_title' . $i, "Slide $i Title");
+            $mform->setDefault('config_item_title' . $i, "Curso $i Título");
             $mform->setType('config_item_title' . $i, PARAM_TEXT);
 
             $mform->addElement('textarea', 'config_item_text' . $i, get_string('config_text', 'block_carousel_educo', $i));
-            $mform->setDefault('config_item_text' . $i, "Slide $i Text");
+            $mform->setDefault('config_item_text' . $i, "Texto de la Diapositiva $i");
             $mform->setType('config_item_text' . $i, PARAM_TEXT);
 
             $mform->addElement('filemanager', 'config_item_image' . $i, get_string('config_image', 'block_carousel_educo', $i), null, array('accepted_types' => array('image')));
+
+            $mform->addElement('text', 'config_item_button' . $i, get_string('config_button', 'block_carousel_educo', $i));
+            $mform->setDefault('config_item_button' . $i, "");
+            $mform->setType('config_item_button' . $i, PARAM_TEXT);
+
+            $mform->addElement('text', 'config_item_link' . $i, get_string('config_link', 'block_carousel_educo', $i));
+            $mform->setDefault('config_item_link' . $i, "");
+            $mform->setType('config_item_link' . $i, PARAM_URL);
         }
     }
 
