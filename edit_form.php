2647e7f5fb36bcd61a61df4ba03dd1b160565895
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-21 22:24:21 -0500

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -10,24 +10,24 @@ class block_carousel_educo_edit_form extends block_edit_form {
             $itemsnumber = $this->block->config->itemsnumber;
         }
 
-        $mform->addElement('header', 'config_header', get_string('blocksettings', 'block'));
+        $mform->addElement('header', 'config_header', get_string('blocksettings', 'block_carousel_educo'));
 
         $itemsrange = range(1, 5);
-        $mform->addElement('select', 'config_itemsnumber', get_string('config_itemsnumber', 'theme_edash'), $itemsrange);
+        $mform->addElement('select', 'config_itemsnumber', get_string('config_itemsnumber', 'block_carousel_educo'), $itemsrange);
         $mform->setDefault('config_itemsnumber', 3);
 
         for ($i = 1; $i <= 5; $i++) {
-            $mform->addElement('header', 'config_edash_item' . $i, get_string('config_item', 'theme_edash') . " $i");
+            $mform->addElement('header', 'config_edash_item' . $i, get_string('config_item', 'block_carousel_educo') . " $i");
 
-            $mform->addElement('text', 'config_item_title' . $i, get_string('config_title', 'theme_edash', $i));
+            $mform->addElement('text', 'config_item_title' . $i, get_string('config_title', 'block_carousel_educo', $i));
             $mform->setDefault('config_item_title' . $i, "Slide $i Title");
             $mform->setType('config_item_title' . $i, PARAM_TEXT);
 
-            $mform->addElement('textarea', 'config_item_text' . $i, get_string('config_text', 'theme_edash', $i));
+            $mform->addElement('textarea', 'config_item_text' . $i, get_string('config_text', 'block_carousel_educo', $i));
             $mform->setDefault('config_item_text' . $i, "Slide $i Text");
             $mform->setType('config_item_text' . $i, PARAM_TEXT);
 
-            $mform->addElement('text', 'config_item_image' . $i, 'Image URL');
+            $mform->addElement('text', 'config_item_image' . $i, get_string('config_image', 'block_carousel_educo', $i));
             $mform->setDefault('config_item_image' . $i, "Image URL for Slide $i");
             $mform->setType('config_item_image' . $i, PARAM_TEXT);
         }
