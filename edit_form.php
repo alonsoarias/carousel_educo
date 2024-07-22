c282b5394668fc13b0198f9c37c5a2886ea12154
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-21 22:26:58 -0500

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -6,7 +6,7 @@ class block_carousel_educo_edit_form extends block_edit_form {
         global $CFG;
 
         $itemsnumber = 3;
-        if(isset($this->block->config->itemsnumber)){
+        if (isset($this->block->config->itemsnumber)) {
             $itemsnumber = $this->block->config->itemsnumber;
         }
 
@@ -27,10 +27,31 @@ class block_carousel_educo_edit_form extends block_edit_form {
             $mform->setDefault('config_item_text' . $i, "Slide $i Text");
             $mform->setType('config_item_text' . $i, PARAM_TEXT);
 
-            $mform->addElement('text', 'config_item_image' . $i, get_string('config_image', 'block_carousel_educo', $i));
-            $mform->setDefault('config_item_image' . $i, "Image URL for Slide $i");
-            $mform->setType('config_item_image' . $i, PARAM_TEXT);
+            $mform->addElement('filemanager', 'config_item_image' . $i, get_string('config_image', 'block_carousel_educo', $i), null, array('accepted_types' => array('image')));
         }
+    }
+
+    public function set_data($defaults) {
+        if (isset($this->block->config)) {
+            $contextid = $this->block->context->id;
+            for ($i = 1; $i <= 5; $i++) {
+                $draftitemid = file_get_submitted_draft_itemid('config_item_image' . $i);
+                file_prepare_draft_area($draftitemid, $contextid, 'block_carousel_educo', 'content', $i, array('subdirs' => false));
+                $defaults->{'config_item_image' . $i} = $draftitemid;
+            }
+        }
+        parent::set_data($defaults);
+    }
+
+    public function get_data() {
+        $data = parent::get_data();
+        if ($data !== null) {
+            $contextid = $this->block->context->id;
+            for ($i = 1; $i <= 5; $i++) {
+                file_save_draft_area_files($data->{'config_item_image' . $i}, $contextid, 'block_carousel_educo', 'content', $i, array('subdirs' => false));
+            }
+        }
+        return $data;
     }
 }
 ?>
