e7932e6bfdd2a861c19b0b83ddafa85aca597a88
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-21 22:47:09 -0500

    Create block_carousel_educo.php

---
+++ lang/es/block_carousel_educo.php
@@ -0,0 +1,18 @@
+<?php
+$string['pluginname'] = '[EduCo] Carrusel Educo';
+$string['carousel_educo'] = '[EduCo] Carrusel Educo';
+$string['blocksettings'] = '[EduCo] Configuración del Bloque Carrusel Educo';
+$string['carousel_educo:addinstance'] = 'Agregar un nuevo bloque [EduCo] Carrusel Educo';
+$string['carousel_educo:myaddinstance'] = 'Agregar un nuevo bloque [EduCo] Carrusel Educo';
+$string['config_itemsnumber'] = 'Número de diapositivas';
+$string['config_item'] = 'Diapositiva';
+$string['config_title'] = 'Título de la diapositiva';
+$string['config_text'] = 'Texto de la diapositiva';
+$string['config_image'] = 'URL de la imagen de la diapositiva';
+$string['config_button'] = 'Texto del botón';
+$string['config_link'] = 'URL del botón';
+$string['previous'] = 'Anterior';
+$string['next'] = 'Siguiente';
+$string['slide'] = 'Diapositiva';
+$string['see_more'] = 'Ver más';
+?>
