5731bab182fc444759bc24658a8803dc1100ebac
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-01 09:17:17 -0500

    Update lib.php

--- lib.php
+++ lib.php
@@ -1,16 +1,24 @@
 <?php
-function block_educo_categories_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = array()) {
-    if ($context->contextlevel == CONTEXT_BLOCK && $filearea === 'slideimage') {
-        $itemid = array_shift($args); // Obtener el itemid
-        $relativepath = implode('/', $args);
-        $fullpath = "/$context->id/block_educo_categories/$filearea/$itemid/$relativepath";
 
-        $fs = get_file_storage();
-        $file = $fs->get_file_by_hash(sha1($fullpath));
-        if (!$file || $file->is_directory()) {
-            send_file_not_found();
-        }
-        send_stored_file($file, 0, 0, $forcedownload, $options);
+function block_carousel_educo_pluginfile($course, $birecordorcm, $context, $filearea, $args, $forcedownload, array $options = array()) {
+    if ($context->contextlevel != CONTEXT_BLOCK) {
+        send_file_not_found();
     }
-    return false;
+
+    if ($filearea !== 'content') {
+        send_file_not_found();
+    }
+
+    $fs = get_file_storage();
+
+    $itemid = array_shift($args);
+    $filename = array_pop($args);
+    $filepath = $args ? '/' . implode('/', $args) . '/' : '/';
+
+    if (!$file = $fs->get_file($context->id, 'block_carousel_educo', $filearea, $itemid, $filepath, $filename) or $file->is_directory()) {
+        send_file_not_found();
+    }
+
+    send_stored_file($file, 60 * 60, 0, $forcedownload, $options);
 }
+?>
