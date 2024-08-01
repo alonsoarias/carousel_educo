c3c484c54595949b87ea5c5c8ea15de3dc975bf6
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-01 12:23:54 -0500

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -10,56 +10,70 @@ class block_carousel_educo_edit_form extends block_edit_form {
             $itemsnumber = $this->block->config->itemsnumber;
         }
 
+        // Add block settings header
         $mform->addElement('header', 'config_header', get_string('blocksettings', 'block_carousel_educo'));
 
-        $itemsrange = range(1, 5);
-        $mform->addElement('select', 'config_itemsnumber', get_string('config_itemsnumber', 'block_carousel_educo'), $itemsrange);
+        // Dropdown to select the number of slides
+        $itemsrange = range(1, 5); // Assuming a max of 5 slides as per the updated requirement
+        $mform->addElement('select', 'config_itemsnumber', get_string('config_items', 'block_carousel_educo'), $itemsrange);
         $mform->setDefault('config_itemsnumber', 1);
 
-        for ($i = 1; $i <= 5; $i++) {
-            $mform->addElement('header', 'config_edash_item' . $i, get_string('config_item', 'block_carousel_educo') . " $i");
+        // Loop to create form fields for each slide
+        for ($i = 1; $i <= $itemsnumber; $i++) {
+            $mform->addElement('header', 'config_educo_item' . $i, get_string('config_item', 'block_carousel_educo') . ' ' . $i);
 
+            // Title
             $mform->addElement('text', 'config_item_title' . $i, get_string('config_title', 'block_carousel_educo', $i));
-            $mform->setDefault('config_item_title' . $i, "Curso $i Título");
+            $mform->setDefault('config_item_title' . $i, "Diapositiva $i Título");
             $mform->setType('config_item_title' . $i, PARAM_TEXT);
 
-            $mform->addElement('textarea', 'config_item_text' . $i, get_string('config_text', 'block_carousel_educo', $i));
+            // Text
+            $mform->addElement('textarea', 'config_item_text' . $i, get_string('config_text', 'block_carousel_educo', $i), 'wrap="virtual" rows="4" cols="50"');
             $mform->setDefault('config_item_text' . $i, "Texto de la Diapositiva $i");
             $mform->setType('config_item_text' . $i, PARAM_TEXT);
 
-            $mform->addElement('filemanager', 'config_item_image' . $i, get_string('config_image', 'block_carousel_educo', $i), null, array('accepted_types' => array('image')));
+            // Image file manager
+            $mform->addElement('filemanager', 'config_item_image' . $i, get_string('config_item_image', 'block_carousel_educo', $i), null, array(
+                'subdirs' => 0,
+                'maxbytes' => $CFG->maxbytes,
+                'areamaxbytes' => 10485760,  // 10MB
+                'maxfiles' => 1,
+                'accepted_types' => array('image')
+            ));
 
+            // Button text
             $mform->addElement('text', 'config_item_button' . $i, get_string('config_button', 'block_carousel_educo', $i));
-            $mform->setDefault('config_item_button' . $i, "");
+            $mform->setDefault('config_item_button' . $i, '');
             $mform->setType('config_item_button' . $i, PARAM_TEXT);
 
+            // Button link
             $mform->addElement('text', 'config_item_link' . $i, get_string('config_link', 'block_carousel_educo', $i));
-            $mform->setDefault('config_item_link' . $i, "");
+            $mform->setDefault('config_item_link' . $i, $CFG->wwwroot . '/');
             $mform->setType('config_item_link' . $i, PARAM_URL);
         }
     }
 
     public function set_data($defaults) {
-        if (isset($this->block->config)) {
-            $contextid = $this->block->context->id;
-            for ($i = 1; $i <= 5; $i++) {
-                $draftitemid = file_get_submitted_draft_itemid('config_item_image' . $i);
-                file_prepare_draft_area($draftitemid, $contextid, 'block_carousel_educo', 'content', $i, array('subdirs' => false));
-                $defaults->{'config_item_image' . $i} = $draftitemid;
-            }
+        $context = context_block::instance($this->block->instance->id);
+        $itemsnumber = $this->block->config->itemsnumber ?? 1;
+
+        for ($i = 1; $i <= $itemsnumber; $i++) {
+            $draftitemid = file_get_submitted_draft_itemid('config_item_image' . $i);
+            file_prepare_draft_area($draftitemid, $context->id, 'block_carousel_educo', 'content', $i, array('subdirs' => 0));
+            $defaults->{'config_item_image' . $i} = $draftitemid;
         }
+
         parent::set_data($defaults);
     }
 
     public function get_data() {
         $data = parent::get_data();
-        if ($data !== null) {
-            $contextid = $this->block->context->id;
-            for ($i = 1; $i <= 5; $i++) {
-                file_save_draft_area_files($data->{'config_item_image' . $i}, $contextid, 'block_carousel_educo', 'content', $i, array('subdirs' => false));
+        if ($data) {
+            $context = context_block::instance($this->block->instance->id);
+            for ($i = 1; $i <= $data->config_itemsnumber; $i++) {
+                file_save_draft_area_files($data->{'config_item_image' . $i}, $context->id, 'block_carousel_educo', 'content', $i, array('subdirs' => 0));
             }
         }
         return $data;
     }
 }
-?>
