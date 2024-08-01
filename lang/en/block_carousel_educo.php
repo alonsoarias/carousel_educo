836cd500ce92531666bb05a2f5c0d50a3bb94874
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-01 12:30:53 -0500

    Update block_carousel_educo.php

--- lang/en/block_carousel_educo.php
+++ lang/en/block_carousel_educo.php
@@ -1,18 +1,31 @@
 <?php
+// This file contains language strings for the Carousel Educo block plugin
+
+// Plugin name and description
 $string['pluginname'] = '[EduCo] Carousel Educo';
 $string['carousel_educo'] = '[EduCo] Carousel Educo';
 $string['blocksettings'] = '[EduCo] Carousel Educo Block Settings';
+
+// Capabilities
 $string['carousel_educo:addinstance'] = 'Add a new [EduCo] Carousel Educo block';
 $string['carousel_educo:myaddinstance'] = 'Add a new [EduCo] Carousel Educo block';
+
+// Configuration settings
 $string['config_itemsnumber'] = 'Number of slides';
-$string['config_item'] = 'Slide';
-$string['config_title'] = 'Title for slide';
-$string['config_text'] = 'Text for slide';
-$string['config_image'] = 'Image URL for slide';
-$string['config_button'] = 'Button text';
-$string['config_link'] = 'Button URL';
+$string['config_item'] = 'Slide {$a}';
+$string['config_title'] = 'Title for slide {$a}';
+$string['config_text'] = 'Text for slide {$a}';
+$string['config_image'] = 'Image for slide {$a}';
+$string['config_button'] = 'Button text for slide {$a}';
+$string['config_link'] = 'Button URL for slide {$a}';
+
+// Navigation and labels
 $string['previous'] = 'Previous';
 $string['next'] = 'Next';
 $string['slide'] = 'Slide';
 $string['see_more'] = 'See more';
+
+// Ensure these match the elements used in edit_form.php
+$string['config_items'] = 'Number of slides'; // Added for dropdown selection in edit_form.php
+$string['config_item_image'] = 'Image for slide'; // Corrected identifier for file manager
 ?>
