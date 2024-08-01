6af4da2b9f640b0d8df808f72a218b148bc62f00
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-01 12:30:56 -0500

    Update block_carousel_educo.php

--- lang/es/block_carousel_educo.php
+++ lang/es/block_carousel_educo.php
@@ -1,18 +1,31 @@
 <?php
+// Este archivo contiene cadenas de idioma para el plugin del bloque Carousel Educo
+
+// Nombre y descripción del plugin
 $string['pluginname'] = '[EduCo] Carrusel Educo';
 $string['carousel_educo'] = '[EduCo] Carrusel Educo';
-$string['blocksettings'] = '[EduCo] Configuración del Bloque Carrusel Educo';
+$string['blocksettings'] = '[EduCo] Configuración del bloque Carrusel Educo';
+
+// Capacidades
 $string['carousel_educo:addinstance'] = 'Agregar un nuevo bloque [EduCo] Carrusel Educo';
 $string['carousel_educo:myaddinstance'] = 'Agregar un nuevo bloque [EduCo] Carrusel Educo';
+
+// Configuración
 $string['config_itemsnumber'] = 'Número de diapositivas';
-$string['config_item'] = 'Diapositiva';
-$string['config_title'] = 'Título de la diapositiva';
-$string['config_text'] = 'Texto de la diapositiva';
-$string['config_image'] = 'URL de la imagen de la diapositiva';
-$string['config_button'] = 'Texto del botón';
-$string['config_link'] = 'URL del botón';
+$string['config_item'] = 'Diapositiva {$a}';
+$string['config_title'] = 'Título para la diapositiva {$a}';
+$string['config_text'] = 'Texto para la diapositiva {$a}';
+$string['config_image'] = 'Imagen para la diapositiva {$a}';
+$string['config_button'] = 'Texto del botón para la diapositiva {$a}';
+$string['config_link'] = 'URL del botón para la diapositiva {$a}';
+
+// Navegación y etiquetas
 $string['previous'] = 'Anterior';
 $string['next'] = 'Siguiente';
 $string['slide'] = 'Diapositiva';
 $string['see_more'] = 'Ver más';
+
+// Asegúrese de que estos coincidan con los elementos utilizados en edit_form.php
+$string['config_items'] = 'Número de diapositivas'; // Añadido para la selección en el formulario de edición
+$string['config_item_image'] = 'Imagen para la diapositiva'; // Identificador corregido para el administrador de archivos
 ?>
