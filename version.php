267a55139550db0854a1f9ac9cdbbf5e8a27d3d2
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-01 08:56:23 -0500

    Update version.php

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 $plugin->component = 'block_carousel_educo';
-$plugin->version = 2023072104;
+$plugin->version = 2023072105;
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 ?>
