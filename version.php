d5ca5edb622b7cb6a583d05fe9e90d533212c56e
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-01 12:32:01 -0500

    Update version.php

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_carousel_educo';
-$plugin->version = 2024080100; // Versión del plugin
+$plugin->version = 2024080101; // Versión del plugin
 $plugin->requires = 2020061500; // Requiere esta versión de Moodle
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
